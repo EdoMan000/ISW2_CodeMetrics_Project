5e1da72bd58ef79824b27b6025656fe88f9b2ecd
EdoMan000 <manenti000@gmail.com>
2023-05-03 15:54:16 +0200

    added starting print... probably will get codesmells :)

--- src/main/java/metrics/Main.java
+++ src/main/java/metrics/Main.java
@@ -8,6 +8,7 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException, GitAPIException, URISyntaxException {
+        ExtractMetrics.logStarting();
         ExtractMetrics.extractDataAndElaborate("BOOKKEEPER","https://github.com/EdoMan000/bookkeeper.git");
         ExtractMetrics.extractDataAndElaborate("OPENJPA", "https://github.com/EdoMan000/openjpa.git");
         ExtractMetrics.logTheEnd();
